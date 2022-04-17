32a08fd5e82433aab1ae26f02bafac539b44572d
ronimizy <ronimizy@niuitmo.ru>
2022-04-17 11:58:00 +0300

    refactor: deleted SourceComponents

--- DiscriminatedUnion.CS/Generators/SourceComponents/IExpressionContainerSourceComponent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using DiscriminatedUnion.CS.Generators.SourceComponents.Components;
-
-namespace DiscriminatedUnion.CS.Generators.SourceComponents;
-
-public interface IExpressionContainerSourceComponent : ISourceComponent, IEnumerable<ExpressionComponent>
-{
-    void Add(ExpressionComponent component);
-}
--- DiscriminatedUnion.CS/Generators/SourceComponents/ISourceComponentVisitor.cs
+++
@@ -1,8 +0,0 @@
-using DiscriminatedUnion.CS.Generators.SourceComponents.Decorators;
-
-namespace DiscriminatedUnion.CS.Generators.SourceComponents;
-
-public interface ISourceComponentVisitor
-{
-    void VisitUsingComponent(UsingComponentDecorator component);
-}
